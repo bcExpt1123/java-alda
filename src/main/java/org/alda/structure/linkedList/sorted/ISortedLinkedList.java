4af4c7668b736550c99de3169810087833336eb3
bcExpt1123 <bcExpt1123@gmail.com>
2025-01-13 20:40:12 -0500

    Add docs into SortedLinkedList

--- src/main/java/org/alda/structure/linkedList/sorted/ISortedLinkedList.java
+++ src/main/java/org/alda/structure/linkedList/sorted/ISortedLinkedList.java
@@ -3,10 +3,53 @@ package org.alda.structure.linkedList.sorted;
 import org.alda.common.Printable;
 import org.alda.structure.linkedList.simple.Node;
 
+/**
+ * @author bcExpt1123
+ *
+ * The {@code ISortedLinkedList} interface defines the essential operations
+ * for a sorted singly linked list, including deleting, searching, and inserting
+ * elements in sorted order.
+ *
+ * <p>This interface extends {@link Printable}, allowing the entire list to be printed.</p>
+ *
+ * @param <T> The type of data stored in the linked list nodes.
+ */
 public interface ISortedLinkedList<T> extends Printable {
+
+    /**
+     * Deletes the first node that contains the specified key.
+     *
+     * @param key The data to delete from the list.
+     * @return The head of the modified list.
+     */
     Node<T> delete(T key);
+
+    /**
+     * Searches for a node containing the specified key.
+     *
+     * @param key The data to search for.
+     * @return {@code true} if a node containing the key is found; {@code false} otherwise.
+     */
     Boolean search(T key);
+
+    /**
+     * Inserts a new node with the specified data into the list while maintaining sorted order.
+     *
+     * @param data The data to insert.
+     */
     void sortedInsert(T data);
+
+    /**
+     * Returns the head (first node) of the linked list.
+     *
+     * @return The head node.
+     */
     Node<T> getHead();
+
+    /**
+     * Sets the head (first node) of the linked list.
+     *
+     * @param head The node to set as the head of the list.
+     */
     void setHead(Node<T> head);
 }
--- src/main/java/org/alda/structure/linkedList/sorted/SortedLinkedList.java
+++ src/main/java/org/alda/structure/linkedList/sorted/SortedLinkedList.java
@@ -4,11 +4,45 @@ import org.alda.common.Comparer;
 import org.alda.structure.linkedList.simple.Node;
 import org.alda.structure.linkedList.simple.SimpleLinkedList;
 
+/**
+ * @author bcExpt1123
+ *
+ * The {@code SortedLinkedList} class implements a sorted singly linked list
+ * where elements are inserted in sorted order. It extends {@link SimpleLinkedList}
+ * and provides an additional method for sorted insertion.
+ *
+ * <p>This class implements the {@link ISortedLinkedList} interface, providing
+ * concrete implementations of its methods.</p>
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ * SortedLinkedList<Integer> sortedList = new SortedLinkedList<>();
+ * sortedList.sortedInsert(10);
+ * sortedList.sortedInsert(5);
+ * sortedList.sortedInsert(20);
+ * sortedList.print();
+ * </pre>
+ *
+ * @param <T> The type of data stored in the list's nodes.
+ * @see SimpleLinkedList
+ * @see ISortedLinkedList
+ * @see Comparer
+ */
 public class SortedLinkedList<T> extends SimpleLinkedList<T> implements ISortedLinkedList<T> {
+
+    /**
+     * Constructs an empty {@code SortedLinkedList}.
+     */
     public SortedLinkedList() {
         super();
     }
 
+    /**
+     * Inserts a new node with the specified data into the list while maintaining sorted order.
+     * The {@code Comparer} utility is used to compare node data.
+     *
+     * @param data The data to insert.
+     */
     public void sortedInsert(T data) {
         Node<T> newNode = new Node<>(data);
         Node<T> current = null;
