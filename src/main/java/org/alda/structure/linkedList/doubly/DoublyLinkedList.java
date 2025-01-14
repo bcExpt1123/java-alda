6ca872ce11da0cb304b9f3e0192ae5eeee11a4fc
bcExpt1123 <bcExpt1123@gmail.com>
2025-01-13 20:32:21 -0500

    Add docs into DoublyLinkedList

--- src/main/java/org/alda/structure/linkedList/doubly/DoublyLinkedList.java
+++ src/main/java/org/alda/structure/linkedList/doubly/DoublyLinkedList.java
@@ -2,15 +2,52 @@ package org.alda.structure.linkedList.doubly;
 
 import org.alda.common.Printable;
 
+/**
+ * @author bcExpt1123
+ *
+ * The {@code DoublyLinkedList} class represents a generic doubly linked list.
+ * Each node in the list has references to both the next and previous nodes,
+ * allowing traversal in both directions.
+ *
+ * <p>This class provides functionality to append, prepend, and print the list's contents.</p>
+ *
+ * <p>Example usage:</p>
+ * <pre>
+ * DoublyLinkedList<Integer> list = new DoublyLinkedList<>();
+ * list.append(10);
+ * list.append(20);
+ * list.prepend(5);
+ * list.print();
+ * </pre>
+ *
+ * @param <T> The type of data stored in the list's nodes.
+ */
 public class DoublyLinkedList<T> implements Printable {
+
+    /**
+     * The head (first node) of the doubly linked list.
+     */
     public Node<T> head;
+
+    /**
+     * The tail (last node) of the doubly linked list.
+     */
     public Node<T> tail;
 
+    /**
+     * Constructs an empty {@code DoublyLinkedList}.
+     * The {@code head} and {@code tail} are initialized to {@code null}.
+     */
     public DoublyLinkedList() {
         head = null;
         tail = null;
     }
 
+    /**
+     * Appends a new node containing the specified data to the end of the list.
+     *
+     * @param data The data to store in the new node.
+     */
     public void append(T data) {
         Node<T> newNode = new Node<T>(data);
         if (head == null) {
@@ -23,6 +60,11 @@ public class DoublyLinkedList<T> implements Printable {
         tail = newNode;
     }
 
+    /**
+     * Prepends a new node containing the specified data to the beginning of the list.
+     *
+     * @param data The data to store in the new node.
+     */
     public void prepend(T data) {
         Node<T> newNode = new Node<>(data);
         if (head == null) {
@@ -35,6 +77,10 @@ public class DoublyLinkedList<T> implements Printable {
         head = newNode;
     }
 
+    /**
+     * Prints the data stored in each node of the doubly linked list from head to tail.
+     * Each node's data is printed along with the data in its next and previous nodes, if available.
+     */
     public void print() {
         Node<T> current = head;
         while (current != null) {
--- src/main/java/org/alda/structure/linkedList/doubly/Node.java
+++ src/main/java/org/alda/structure/linkedList/doubly/Node.java
@@ -2,16 +2,51 @@ package org.alda.structure.linkedList.doubly;
 
 import org.alda.common.Printable;
 
+/**
+ * @author bcExpt1123
+ *
+ * The {@code Node} class represents a single element in a doubly linked list.
+ * Each node stores a piece of data, a reference to the next node, and a reference to the previous node.
+ *
+ * <p>This class also implements the {@link Printable} interface, allowing the node's content
+ * to be printed in a human-readable format.</p>
+ *
+ * @param <T> The type of data stored in the node.
+ *
+ */
 public class Node<T> implements Printable {
+
+    /**
+     * The data stored in this node.
+     */
     public T data;
+
+    /**
+     * The reference to the next node in the list.
+     */
     public Node<T> next;
+
+    /**
+     * The reference to the previous node in the list.
+     */
     public Node<T> prev;
 
+    /**
+     * Constructs a new {@code Node} with the specified data.
+     * The {@code next} and {@code prev} references are initialized to {@code null}.
+     *
+     * @param data The data to store in the node.
+     */
     public Node(final T data) {
         this.data = data;
         next = null;
         prev = null;
     }
+
+    /**
+     * Prints the data stored in this node, as well as the data in the next and previous nodes, if available.
+     * The output format is: {@code Data: <data>, Next: <next data>, Prev: <prev data>}
+     */
     public void print() {
         System.out.print("Data: " + data);
         if (next != null) {
