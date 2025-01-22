80a143f0770ddd7ede8330cad9c277ce6ffb2b07
bcExpt1123 <bcExpt1123@gmail.com>
2025-01-21 20:34:27 -0500

    Add Stack using ArrayList

---
+++ src/main/java/org/alda/structure/stack/IStackArray.java
@@ -0,0 +1,13 @@
+package org.alda.structure.stack;
+
+/**
+ * @author bcExpt1123
+ * @param <T>
+ */
+public interface IStackArray<T> {
+    boolean isEmpty(); // Check if the stack is empty
+    int size();        // Get the size of the stack
+    void push(T t);    // Push an element onto the stack
+    T pop();           // Pop an element from the stack
+    T peek();          // Peek at the top element of the stack
+}
---
+++ src/main/java/org/alda/structure/stack/StackArray.java
@@ -0,0 +1,36 @@
+package org.alda.structure.stack;
+
+import java.util.ArrayList;
+
+/**
+ * @author bcExpt1123
+ * Stack using ArrayList
+ * @param <T>
+ */
+public class StackArray<T> implements IStackArray<T> {
+    private final ArrayList<T> stack;
+    public StackArray() {
+        stack = new ArrayList<>();
+    }
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+    public int size() {
+        return stack.size();
+    }
+    public void push(T t) {
+        stack.add(t);
+    }
+    public T pop() {
+        if (isEmpty()){
+            return null;
+        }
+        return stack.removeLast();
+    }
+    public T peek() {
+        if (isEmpty()){
+            return null;
+        }
+        return stack.getLast();
+    }
+}
